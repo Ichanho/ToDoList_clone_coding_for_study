5e20dffbc838990dc074d0341023d57902f0ab83
LEE CHAN HO <supprpass15@gmail.com>
2022-08-20 02:30:12 +0900

    Feat login function
    Login function made by localstorage and form
    If you want, change your name to click buttom

--- src/login.js
+++ src/login.js
@@ -1,13 +1,45 @@
-const body = document.querySelector("body")
-const userName = document.querySelector(".userName");
+// Define string
+const HIDDEN_CLASS = "hidden";
+const GET_USER_NAME = "getUserName";
+
+// Call html tag
+const body = document.querySelector("body");
+const getUserName = document.querySelector(".getUserName");
+const userNameInput = document.querySelector(".getUserName input");
+const helloUser = document.querySelector(".helloUser");
+const helloSpan = document.querySelector(".helloUser span");
+const changeName = document.querySelector(".changeName");
+
+// 초기 저장된 username 확인
+let saveUserName = localStorage.getItem(GET_USER_NAME);
+
+
 
 function handleUserName(event) {
   event.preventDefault();
   
-  userName.style = "hidden";
-  console.dir(userName);
+  getUserName.classList.add(HIDDEN_CLASS);
+  localStorage.setItem(GET_USER_NAME, userNameInput.value);
+
+  helloUser.classList.remove(HIDDEN_CLASS);
+  saveUserName = localStorage.getItem(GET_USER_NAME);
+  helloSpan.innerText=("Hellow "+saveUserName);
+  // console.dir(userName);
 }
 
+function handelChangeName(event){
+  getUserName.classList.remove(HIDDEN_CLASS);
+  helloUser.classList.add(HIDDEN_CLASS);
+  localStorage.removeItem(GET_USER_NAME);
+}
 
+if(saveUserName != null)
+{
+  getUserName.classList.add(HIDDEN_CLASS);
+  helloUser.classList.remove(HIDDEN_CLASS);
+  helloSpan.innerText=("Hello "+saveUserName);
+}
 
-userName.addEventListener("submit",handleUserName)
+getUserName.addEventListener("submit",handleUserName);
+
+changeName.addEventListener("click", handelChangeName);
