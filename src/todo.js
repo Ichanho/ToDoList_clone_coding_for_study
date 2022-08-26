021150c899400ae1886b2bb44676e2e906e16957
LEE CHAN HO <supprpass15@gmail.com>
2022-08-26 16:08:23 +0900

    Feat save/delete todo list by localstorage
    by array

--- src/todo.js
+++ src/todo.js
@@ -2,21 +2,50 @@ const toDoForm = document.querySelector(".toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoShow = document.querySelector(".toDoShow");
 
-const TODO_ITEM = "toDoItem"
 
-let count = 1
+const TODO_KEY = "toDoItem"
 
-/**몇번째 태그인지 Class 명을 통해 저장 후 li 추가 */
-function showToDoList(number)
+let toDos = [];
+
+
+function handleToDoDelete(event)
+{
+  const li = event.target.parentElement;
+  const deleteID = parseInt(event.target.parentNode.id);
+
+  function filteringItem(item){
+    console.log('ID :'+deleteID+'type : '+typeof(deleteID));
+    console.log('--ID :'+item.id+'type : '+typeof(item.id));
+    return item.id !== deleteID}
+
+  const tempToDos = toDos.filter(filteringItem);
+  toDos = tempToDos;
+  console.log(toDos);
+  saveToDo();
+
+  li.remove();
+}
+
+/**각각 ID 부여 후 문자 출력, Delete 버튼 부여 */
+function showToDoList(toDo)
 {
   let newToDo = document.createElement("li");
-  newToDo.setAttribute("class", number);
+  newToDo.setAttribute("id", toDo.id);
   let newToDoText = document.createElement("span");
-  newToDoText.innerText = localStorage.getItem(TODO_ITEM);
-  let newToDoDelte = document.createElement("button");
-  
+  newToDoText.innerText = toDo.text;
+  let newToDoDelete = document.createElement("button");
+  newToDoDelete.innerText = "X";
+  newToDoDelete.addEventListener("click",handleToDoDelete);
 
+  newToDo.appendChild(newToDoText);
+  newToDo.appendChild(newToDoDelete);
   toDoShow.appendChild(newToDo);
+}
+
+/**Save 'toDos' to localStorage */
+function saveToDo(){
+  const toDosString = JSON.stringify(toDos);
+  localStorage.setItem(TODO_KEY, toDosString);
 }
 
 /** */
@@ -24,12 +53,29 @@ function handleToDoForm(event)
 {
   event.preventDefault();
 
-  localStorage.setItem(TODO_ITEM, toDoInput.value);
+  const toDoObj = {
+    id: Date.now(),
+    text: toDoInput.value
+  };
   toDoInput.value = null;
-  showToDoList(count);
 
-  count +=1;
+  toDos.push(toDoObj);
+  showToDoList(toDoObj);
+  saveToDo();
 }
 
-
 toDoForm.addEventListener("submit", handleToDoForm);
+
+
+const savedToDo = localStorage.getItem(TODO_KEY);
+
+function loadingToDo(iTem)
+{
+  toDos.push(iTem);
+  showToDoList(iTem);
+}
+
+if(savedToDo){
+  const parsedToDos = JSON.parse(savedToDo);
+  parsedToDos.forEach(loadingToDo);
+}
